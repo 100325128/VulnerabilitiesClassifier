e1c33e2afcb40fecf61d7e5f74c71c816cafe5ae
100325128 <100325128@alumnos.uc3m.es>
2016-08-10 19:33:05 +0200

    Update CommonFunctions.py

--- CLASSIFIER/CommonFunctions.py
+++ CLASSIFIER/CommonFunctions.py
@@ -7,24 +7,15 @@ import shutil
 import OSClassify
 import OSTypes
 
-#constante que identifica que el contenido del producto es una vulnerabilidad a nivel de SO
 CONST_IS_OS="cpe:/o:"
-#constante que identifica que el contenido del producto es una vulnerabilidad a nivel de aplicación
 CONST_IS_AP="cpe:/a:"
-#constante que identifica que el contenido del producto es una vulnerabilidad a nivel de HW
 CONST_IS_HW="cpe:/h:"
 
-#esta función agrega informacion de hw a la lista de hw vulnerable
 def addToVulnerableList(vulnerable_list, info) :
     if (info not in vulnerable_list) :
        vulnerable_list += [info]
     return vulnerable_list
 
-#isOS -> True si el parámetro productAux hace referencia a un Sistema Operativo y False eoc
-#isAP -> True si el parámetro productAux hace referencia a una aplicacion y False eoc
-#isHW -> True si el parámetro productAux hace referencia a hardware y False eoc
-#info -> devuelve la información asociada al SO, a la aplicación o al hw
-#ejemplo -> cpe:/o:microsoft:windows_95:fff -> microsoft:windows_95:fff
 def classifyProduct(productAux) :
     isOS = False
     isAP = False
@@ -42,16 +33,9 @@ def classifyProduct(productAux) :
 
     return isOS,isAP,isHW,info
 
-#tuplaResultado contiene como primer valor el nombre del fabricante y
-#segundo valor el nombre de la aplicación
-#ejemplo -> netscape:messaging_server:3.55 -> (netscape,messaging server)
 def processApplication(infoProduct) :
-    #dividimos la cadena separando por dos puntos
     partition = infoProduct.split(":")
     tuplaResultado = ("","")
-    #comprobamos que se el texto contiene el fabricante y la aplicacion
-    #en caso de no tener alguno de ellos se añade una nota para revisar
-    #reemplazamos los _ por espacio
     if(len(partition) < 2) :
        if len(partition) == 1 :
           tuplaResultado[0] = partition[0].replace("_"," ")
